6473597995859426e7c5ba0688c415ae3979ef4b
Thearithhoul <Thearithhoul@gmail.com>
2024-10-18 21:09:04 +0700

    create functons generateupload url

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,7 +1,9 @@
 import * as functions from "firebase-functions/v1";
-import { initializeApp } from "firebase-admin/app";
-import { Firestore } from "firebase-admin/firestore";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -9,15 +11,45 @@ import * as logger from "firebase-functions/logger";
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+
+
+const rawVideoBucketName = "rith-yt-raw-video";
+
 
 export const createUser = functions.auth.user().onCreate((user) => {
-    const userInfo = {
-        uid: user.uid,
-        email: user.email,
-        photoUrl: user.photoURL,
-    };
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
 
-    firestore.collection("users").doc(user.uid).set(userInfo);
-    logger.info(`User Created: ${JSON.stringify(userInfo)}`);
-    return;
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+  return;
 });
+
+
+export const generateUploadUrl = onCall(
+  {maxInstances: 1}, async (request) => {
+    // Check if the user if authenticated
+    if (!request.auth) {
+      throw new functions.https.HttpsError("failed-precondition",
+        "The function must be called while authenticated.");
+    }
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    // Generate a unique filename
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+    // Get a v4 signed URL for uploading file
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+
+    });
+    return {url, fileName};
+  });
